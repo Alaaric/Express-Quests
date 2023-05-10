4c4a6432948043178ec1d1c2aa138ad16cd37d57
Alaric HENROT <alaric.henrot@gmail.com>
2023-05-10 11:23:03 +0200

    fix

--- validators.js
+++ validators.js
@@ -2,27 +2,27 @@ const validateMovie = (req, res, next) => {
     const { title, director, year, color, duration } = req.body;
     const errors = [];
   
-    if (title == null) {
+    if (title === null) {
       errors.push({ field: "title", message: "This field is required" });
-    } else if (title.length >= 255) {
+    } else if (title.length > 255) {
       errors.push({ field: "title", message: "Should contain less than 255 characters" });
     }
-    if (director == null) {
+    if (director === null) {
       errors.push({ field: "director", message: "This field is required" });
-    } else if (director.length >= 255) {
+    } else if (director.length > 255) {
       errors.push({ field: "director", message: "Should contain less than 255 characters" });
     }
-    if (year == null) {
+    if (year === null) {
       errors.push({ field: "year", message: "This field is required" });
-    } else if (year.length >= 255) {
+    } else if (year.length > 255) {
       errors.push({ field: "year", message: "Should contain less than 255 characters" });
     }
-    if (color == null) {
+    if (color === null) {
       errors.push({ field: "color", message: "This field is required" });
-    } else if (color.length >= 255) {
+    } else if (color.length > 255) {
       errors.push({ field: "color", message: "Should contain less than 255 characters" });
     }
-    if (duration == null) {
+    if (duration === null) {
       errors.push({ field: "duration", message: "This field is required" });
     } else if (isNaN(duration)) {
       errors.push({ field: "duration", message: "Should contain less than 255 characters" });
@@ -41,13 +41,15 @@ const validateMovie = (req, res, next) => {
     email: Joi.string().email().max(255).required(),
     firstname: Joi.string().max(255).required(),
     lastname: Joi.string().max(255).required(),
+    city: Joi.string().max(255).required(),
+    language: Joi.string().max(255).required(),
   });
   
   const validateUser = (req, res, next) => {
-    const { firstname, lastname, email } = req.body;
+    const { firstname, lastname, email, city, language } = req.body;
   
     const { error } = userSchema.validate(
-      { firstname, lastname, email },
+      { firstname, lastname, email, city, language },
       { abortEarly: false }
     );
   
